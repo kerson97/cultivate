9910d85f6847ddda8a812d2abe2deb86d05d6d6f
carson <carson_97@live.ca>
2023-04-30 11:14:32 -0700

    First Commit
    -login, register, navbar

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom'
+import './login.scss'
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <div className='card'>
+        <div className='left'>
+          <div className='headers'>
+            <h1>Cultivate</h1>
+          </div>
+          <span>Don't have an account?</span>
+          <Link to='/register'>
+            <button>Register</button>
+          </Link>
+        </div>
+
+        <div className='right'>
+          <h1>Login</h1>
+          <form>
+            <input type='text' placeholder='Username' />
+            <input type='password' placeholder='Password' />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
